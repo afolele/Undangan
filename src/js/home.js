8c43ba49add4fb2249c52ebef93d982197df8546
Jaka <33797566+afolele@users.noreply.github.com>
2025-04-13 14:23:53 +0700

    Update home.js

--- src/js/home.js
+++ src/js/home.js
@@ -6,7 +6,7 @@ export const home = () => {
     const [_, figureElement, timeElement, homeTime, calendarAnchor] = homeContainer.children;
 
     const generateFigureContent = ({bride}) => {
-        const {L: {name: Jaka}, P: {name: Dian}, couple: coupleImage} = bride;
+        const {L: {name: brideLName}, P: {name: bridePName}, couple: coupleImage} = bride;
         return `
             <img src="${coupleImage}" alt="couple animation">
             <figcaption>
